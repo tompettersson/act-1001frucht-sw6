68ad383269f5906924a6d511a9b0791ba521f524
tompettersson <thomas.pettersson@actualize.de>
2022-07-28 18:09:07 +0200

    Initial commit

---
+++ custom/plugins/Act1001FruchtTheme/src/Act1001FruchtTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Act1001FruchtTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class Act1001FruchtTheme extends Plugin implements ThemeInterface
+{
+}
